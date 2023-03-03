24933572a172fff0fa0d4fc3eca82387e8d4088b
caleb beam <caleb@LAPTOP-IC1KTCC2>
2023-03-03 17:35:36 -0600

    second step

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-(function() {
-  'use strict';
+var goBtn = document.getElementById('goButton')
+var slowBtn = document.getElementById('slowButton')
+var stopBtn = document.getElementById('stopButton')
 
-  // YOUR CODE HERE
-})();
+var goBulb = document.getElementById('goLight')
+var slowBulb = document.getElementById('slowLight')
+var stopBulb = document.getElementById('stopLight')
+
+goBtn.addEventListener("click", function() {
+  goBulb.classList.toggle('go')
+});
+slowBtn.addEventListener("click", function() {
+  slowBulb.classList.toggle('slow')
+});
+stopBtn.addEventListener("click", function() {
+  stopBulb.classList.toggle('stop')
+});
+
+function buttonLog(buttonName){
+  console.log(`${buttonName} button`)
+}
+
+let buttons = document.querySelectorAll(' .button')
+
+for(var i = 0; i < buttons.length; i++){
+  buttons[i].addEventListener('mouseenter', (event) => {
+    buttonLog('Entered ' + event.target.innerText)
+    })
+}
+
+for(var i = 0; i < buttons.length; i++){
+  buttons[i].addEventListener('mouseleave', (event) => {
+      buttonLog('Left ' + event.target.innerText)
+    })
+}
